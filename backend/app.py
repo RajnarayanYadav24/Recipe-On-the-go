15eb8e6b65b0a96b640dcd524a6223b7ef2d5dbd
RAJNARAYAN YADAV <rajnarayannit22@gmail.com>
2026-04-09 22:57:09 +0530

    home api created

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
 # Hello and welcome to the backend app
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/home", methods=["GET"])
+def home():
+  return "Welcome to the homepage"
+
+
+if __name__=="__main__":
+  app.run(debug=True)
